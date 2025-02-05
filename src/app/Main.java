4142ab0267469e03cb39687b29cb8aa0955f0e32
KovalenkoBogdan <kbohdan45@gmail.com>
2025-02-05 20:21:37 +0200

    Add converter fahrenheit to Celsius

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,5 +3,12 @@ package app;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Converter App");
+        double far = 150;
+        double cel = converterFarToCel(far);
+        System.out.println("Result is " + cel + " celsiy");
+    }
+
+    private static double converterFarToCel(double far) {
+        return (far - 32) * 5 / 9;
     }
 }
